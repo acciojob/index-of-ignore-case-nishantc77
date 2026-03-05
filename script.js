6c066f4b092b6f4cd6fba6d901642abf693a964a
nishantc77 <157980448+nishantc77@users.noreply.github.com>
2026-03-05 21:43:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+  // write your code hereif (subStr.length === 0) return 0;
+    // Convert both strings to lowercase and use indexOf
+    return str.toLowerCase().indexOf(subStr.toLowerCase());
+}
+
+// Examples
+console.log(indexOfIgnoreCase("Hello World", "world")); // 6
+console.log(indexOfIgnoreCase("apple", "Ple"));        // 2
+console.log(indexOfIgnoreCase("test", "aaa"));         // -1
+console.log(indexOfIgnoreCase("abc", "")); 
 }
 
 // Please do not change the code below
